d47cbce9659f18991bc8cebaf27a4f997ee42581
Milan Panik <milan.panik1@gmail.com>
2019-11-05 14:30:10 +0100

    DISCO-2456 Add passport-visa.service

--- projects/ddap-common-lib/src/public-api.ts
+++ projects/ddap-common-lib/src/public-api.ts
@@ -13,6 +13,7 @@ export * from './lib/form/form';
 export * from './lib/form/form-validation.service';
 export * from './lib/form/validators';
 export * from './lib/visa-passports/ddap-visa-passport.module';
+export * from './lib/visa-passports/visa-passport.service';
 export * from './lib/store/store';
 export * from './lib/error-handler/error-handler.service';
 export * from './lib/time-duration.parser';
