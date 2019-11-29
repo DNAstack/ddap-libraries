c298afc28f09f2e9cfe518f4fb0c0b9073db5832
Milan Panik <milan.panik1@gmail.com>
2019-11-29 12:32:34 +0100

    Add additional form methods for control manipulation

--- projects/ddap-common-lib/src/lib/form/form.ts
+++ projects/ddap-common-lib/src/lib/form/form.ts
@@ -19,4 +19,37 @@ export function combineForms(...forms: Form[]): Form {
   };
 }
 
+export function removeInternalFields(model: { [ key: string]: string }, fieldsToRemove: string[]): { [ key: string]: string } {
+  Object.values(model)
+    .forEach((value: any) => {
+      Object.entries(value)
+        .forEach(([innerKey, innerValue]: any) => {
+          if (innerValue instanceof Object) {
+            return this.removeInternalFields(innerValue, fieldsToRemove);
+          }
+          if (fieldsToRemove.some((fieldToRemove) => fieldToRemove === innerKey)) {
+            delete value[innerKey];
+          }
+        });
+    });
+  return model;
+}
+
+export function alignControlsWithModelDefinitions(formGroups: FormGroup[]) {
+  formGroups.forEach((formGroup) => {
+    Object.entries(formGroup.controls)
+      .forEach(([currentControlId, control]: any) => {
+        const { id: newControlId } = control.value;
+        this.changeControlId(formGroup, currentControlId, newControlId);
+      });
+  });
+}
+
+function changeControlId(formGroup: FormGroup, currentId, newId) {
+  if (currentId !== newId) {
+    formGroup.addControl(newId, formGroup.get(currentId));
+    formGroup.removeControl(currentId);
+  }
+}
+
 export default Form;
