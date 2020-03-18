84dad78fefdcda8786d6e4b98bc036887816fdb8
Marc Fiume <marc@dnastack.com>
2020-03-18 11:34:50 -0400

    Added logo to header, moved terms to footer

---
+++ projects/ddap-common-lib/src/lib/layout/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'ddaplib-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
