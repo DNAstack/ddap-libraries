858d5433a90eb9037d284559daa23e3e34736ffe
Marc Fiume <marc@dnastack.com>
2020-04-21 23:57:45 -0400

    Add action to cover message

--- projects/ddap-common-lib/src/lib/layout/cover-message/cover-message.component.ts
+++ projects/ddap-common-lib/src/lib/layout/cover-message/cover-message.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'ddaplib-cover-message',
@@ -9,6 +9,13 @@ export class CoverMessageComponent {
 
     @Input() icon: string;
     @Input() message: string;
+    @Input() buttonLabel: string;
+    @Input() buttonIcon: string;
+    @Output() onButtonClick: EventEmitter<any> = new EventEmitter();
+
+    onClick(): void {
+        this.onButtonClick.emit();
+    }
 
     constructor(
     ) {
