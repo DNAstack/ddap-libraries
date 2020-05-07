64a85e04e40f9a704c45cc0c01ceb3fca1824c2c
Monica <monica@dnastack.com>
2020-05-07 17:51:37 -0400

    DISCO-2845 Added disable in mobile option in main component

--- projects/ddap-common-lib/src/lib/layout/main/main.component.ts
+++ projects/ddap-common-lib/src/lib/layout/main/main.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {BreakpointObserver} from "@angular/cdk/layout";
 
 @Component({
   selector: 'ddaplib-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
+export class MainComponent implements OnInit{
+
+  @Input()
+  disableMobile?: boolean = false;
+  @Input()
+  maxWidth?: string = '599px';
+  isMobile: boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.isMobile = this.breakpointObserver.isMatched(`(max-width: ${this.maxWidth})`)
+  }
 }
