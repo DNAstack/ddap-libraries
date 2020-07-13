7383b4b1e6407761dd13e8c5df8a5b58f19a15db
Milan Panik <milan.panik1@gmail.com>
2020-07-13 11:50:07 +0200

    DISCO-2918 Explicitelly export components (required in v10)

--- projects/ddap-common-lib/src/public-api.ts
+++ projects/ddap-common-lib/src/public-api.ts
@@ -3,25 +3,46 @@
  */
 
 export * from './lib/admin/ddap-admin.module';
+export * from './lib/admin/entity-description/entity-description.component';
+export * from './lib/admin/entity-description/entity-description-link.component';
 export * from './lib/admin/config-modification.model';
 export * from './lib/admin/entity.model';
+
 export * from './lib/layout/ddap-layout.module';
-export * from './lib/layout/sandbox-banner/sandbox-banner-state.service';
 export * from './lib/layout/realm/realm.constant';
 export * from './lib/layout/realm/realm-state.service';
 export * from './lib/layout/realm/realm.guard';
+export * from './lib/layout/realm/realm-input/realm-input.component';
 export * from './lib/layout/realm/realm-action-confirmation-dialog/realm-action-confirmation-dialog.model';
+export * from './lib/layout/header/header.component';
+export * from './lib/layout/header/header-btn/header-btn.component';
+export * from './lib/layout/header/header-add-btn/header-add-btn.component';
+export * from './lib/layout/header/searchable-header.component';
+export * from './lib/layout/landing-info-card/landing-info-card.component';
+export * from './lib/layout/main/main.component';
+export * from './lib/layout/menu/menu.module';
+export * from './lib/layout/menu/menu.component';
+export * from './lib/layout/menu/menu-apps/menu-apps.component';
+export * from './lib/layout/menu/menu-profile/menu-profile.component';
+export * from './lib/layout/sandbox-banner/sandbox-banner.component';
+export * from './lib/layout/sandbox-banner/sandbox-banner-state.service';
+export * from './lib/layout/sidenav-left/sidenav-left.component';
+export * from './lib/layout/cover-message/cover-message.component';
+export * from './lib/layout/image-preview/image-preview.component';
+export * from './lib/layout/footer/footer.component';
+
 export * from './lib/form/ddap-form.module';
 export * from './lib/form/form';
 export * from './lib/form/form-validation.service';
 export * from './lib/form/validators';
+export * from './lib/form/form-footer-error/form-footer-error.component';
+export * from './lib/form/form-field-btn-group/form-field-btn-group.component';
+export * from './lib/form/form-inline-editable-table/form-inline-editable-table.component';
+
 export * from './lib/store/store';
 export * from './lib/error-handler/error-handler.service';
 export * from './lib/time-duration.parser';
 export * from './lib/array-utils';
 export * from './lib/text-utils';
-
-export * from './lib/layout/menu/menu.module';
-
 export * from './lib/view-controller/view-controller.service';
 export * from './lib/view-controller/module-metadata.model';
