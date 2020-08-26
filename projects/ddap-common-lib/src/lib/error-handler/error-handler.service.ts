5b49dbf9f9a30527be53ac41a2aa8ea02d621273
Milan Panik <milan.panik1@gmail.com>
2020-08-26 21:44:17 +0200

    [#174220713] Add support for log in again snackbar

--- projects/ddap-common-lib/src/lib/error-handler/error-handler.service.ts
+++ projects/ddap-common-lib/src/lib/error-handler/error-handler.service.ts
@@ -3,18 +3,26 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable, OperatorFunction } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import _get from 'lodash.get';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ErrorHandlerService {
 
-  constructor(private snackBar: MatSnackBar) {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private snackBar: MatSnackBar
+  ) {
   }
 
-  public notifyOnError<T>(message?: string): OperatorFunction<T, T> {
+  public notifyOnError<T>(message?: string, authRequired?: boolean): OperatorFunction<T, T> {
     return catchError<T, Observable<T>>((error) => {
-      this.openSnackBar(message, error);
+      if (authRequired && error.status === 401) {
+        this.logInAgainSnackBar('Your login session has expired. You will not be able to perform any actions until you log in again.');
+      } else {
+        this.openSnackBar(message, error);
+      }
       throw error;
     });
   }
@@ -32,5 +40,17 @@ export class ErrorHandlerService {
       panelClass: 'ddap-error',
     });
   }
+
+  private logInAgainSnackBar(message?: string) {
+    this.snackBar.open(message, "Log in", {
+      panelClass: 'ddap-error',
+    }).onAction()
+      .subscribe(() => {
+        this.activatedRoute.root.firstChild.params.subscribe((params) => {
+          window.location.href = `/api/v1alpha/realm/${params.realmId}/identity/login`;
+        });
+      });
+  }
+
 
 }
